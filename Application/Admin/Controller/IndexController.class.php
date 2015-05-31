ef97de4f809d20dea74326898eae10183097db74
KuoKongQingYun <609898048@qq.com>
2015-06-01 02:45:27 +0800

    备份进度
    备份进度

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -3,6 +3,12 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+    	$user=M('user');
+    	$order=M('order');
+    	$item=M('item');
+    	$this->assign('ItemCount',$item->Count());
+    	$this->assign('OrderCount',$order->Count());
+    	$this->assign('UserCount',$user->Count());
         $this->display();
     }
 }
